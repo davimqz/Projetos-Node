05c252bec15438e3fd606daceaf2315e7674ecd8
Davi <dmo3@cesar.school>
2026-03-29 14:10:47 -0300

    [Feat] - Cart.js signature

---
+++ Bootcamp-DIO/06-shopee-cart/src/services/cart.js
@@ -0,0 +1,22 @@
+//Adicionar Item
+async function addItem (userCart) {
+
+}
+
+
+//Deletar Item
+async function deleteItem (userCart, name) {
+
+}
+
+
+//Remover um Item
+async function removeItem (userCart, index) {
+
+}
+
+
+//Calcular total
+async function calculateTotal (userCart) {
+    
+}
